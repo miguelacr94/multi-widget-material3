ab791a313e42f07003a7268cc9b456b05421b5ae
Miguel C <miguelacr94@gmail.com>
2026-03-13 16:56:12 -0500

    Snackbar terminado

--- lib/presentations/screens/progress/progress_screen.dart
+++ lib/presentations/screens/progress/progress_screen.dart
@@ -8,12 +8,64 @@ class ProgressScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Progress'),
-      ),
-      body: Center(
-        child: Text('Hola desde Progress!'),
-      ),
+      appBar: AppBar(title: Text('Progress')),
+      body: Center(child: _ProgressView()),
+    );
+  }
+}
+
+class _ProgressView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 30),
+        Text('Circular Progress'),
+        const SizedBox(height: 20),
+        const CircularProgressIndicator(
+          strokeWidth: 2,
+          backgroundColor: Colors.black45,
+        ),
+        const SizedBox(height: 30),
+        Text('Linear Progress'),
+        const SizedBox(height: 20),
+        _ControllerProgressIndicator(),
+      ],
+    );
+  }
+}
+
+class _ControllerProgressIndicator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Stream.periodic(
+        const Duration(milliseconds: 300),
+        (value) => (value * 2) / 100,
+      ).takeWhile((value) => value < 100),
+      builder: (context, asyncSnapshot) {
+        final progressValue = asyncSnapshot.data ?? 0;
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(
+                value: progressValue,
+                strokeWidth: 2,
+                backgroundColor: Colors.black12,
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: LinearProgressIndicator(
+                  value: progressValue,
+                  backgroundColor: Colors.black45,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
--- lib/presentations/screens/snackbar/snackbar_screen.dart
+++ lib/presentations/screens/snackbar/snackbar_screen.dart
@@ -1,18 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   const SnackbarScreen({super.key});
-  
+
   static const String routeName = 'snackbar';
+
+  void showSnackbar(BuildContext context) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    final snackBar = SnackBar(
+      content: Text('Hello World'),
+      duration: Duration(seconds: 3),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void openDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        title: Text('¿Estás seguro?'),
+        content: Text('Esta acción no se puede deshacer'),
+        actions: [
+          TextButton(
+            onPressed: () => context.pop(),
+            child: Text('Cancelar'),
+          ),
+          TextButton(
+            onPressed: () => context.pop(),
+            child: Text('Aceptar'),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('SnackbarScreen'),
-      ),
+      appBar: AppBar(title: Text('SnackbarScreen')),
       body: Center(
-        child: Text('Hola desde SnackbarScreen!'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+              child: Text('Licencias usadas'),
+              onPressed: () => showAboutDialog(
+                context: context,
+                children: [
+                  const Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 16),
+
+            FilledButton.tonal(
+              onPressed: () => openDialog(context),
+              child: Text('Mostrar diálogos'),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => showSnackbar(context),
+        label: Text('Show Snackbar'),
+        icon: Icon(Icons.access_alarm_outlined),
       ),
     );
   }
