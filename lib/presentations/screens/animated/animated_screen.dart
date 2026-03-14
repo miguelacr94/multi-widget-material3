589675e9dfe0f1cad79167560dbf10ebdbde2b22
Miguel C <miguelacr94@gmail.com>
2026-03-14 09:35:18 -0500

    Ui Screen finish

--- lib/presentations/screens/animated/animated_screen.dart
+++ lib/presentations/screens/animated/animated_screen.dart
@@ -1,18 +1,59 @@
 import 'package:flutter/material.dart';
+import 'dart:math' show Random;
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
   static const String routeName = 'animated_screen';
   const AnimatedScreen({super.key});
 
   @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+  double width = 50;
+  double height = 50;
+  Color color = Colors.indigo;
+  double borderRadius = 10.0;
+
+  void changeShape() {
+    final random = Random();
+    setState(() {
+      width = random.nextInt(300).toDouble() + 50;
+      height = random.nextInt(300).toDouble() + 50;
+      color = Color.fromRGBO(
+        random.nextInt(256),
+        random.nextInt(256),
+        random.nextInt(256),
+        1,
+      );
+      borderRadius = random.nextInt(100).toDouble() + 10;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Animated Screen'),
-      ),
+      appBar: AppBar(title: Text('Animated Screen')),
       body: Center(
-        child: Text('Hola desde Animated Screen!'),
+        child: Center(
+          child: AnimatedContainer(
+            duration: Duration(milliseconds: 400),
+            curve: Curves.easeOutCubic,
+            width: width <= 0 ? 0 : width,
+            height: height <= 0 ? 0 : height,
+            decoration: BoxDecoration(
+              color: color,
+              borderRadius: BorderRadius.circular(borderRadius < 0 ? 0 : borderRadius),
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: changeShape,
+        child: Icon(Icons.play_arrow),
       ),
     );
   }
 }
+
+
--- lib/presentations/screens/ui_controls/ui_controls.dart
+++ lib/presentations/screens/ui_controls/ui_controls.dart
@@ -1,19 +1,124 @@
 import 'package:flutter/material.dart';
 
-class UiControls extends StatelessWidget {
+class UiControls extends StatefulWidget {
   const UiControls({super.key});
 
   static const String routeName = 'ui-controls';
 
   @override
+  State<UiControls> createState() => _UiControlsState();
+}
+
+class _UiControlsState extends State<UiControls> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('UiControls'),
-      ),
-      body: Center(
-        child: Text('Hola desde UiControls!'),
-      ),
+      appBar: AppBar(title: Text('UiControls')),
+      body: _UiControlsView(),
+    );
+  }
+}
+
+class _UiControlsView extends StatefulWidget {
+  @override
+  State<_UiControlsView> createState() => _UiControlsViewState();
+}
+
+enum Transportation { car, bike, bus }
+
+class _UiControlsViewState extends State<_UiControlsView> {
+  bool _switchValue = true;
+  bool _wantsBreakfast = false;
+  bool _wantsLunch = false;
+  bool _wantsDinner = false;
+
+  Transportation? _selectedTransportation = Transportation.car;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      physics: const ClampingScrollPhysics(),
+      children: [
+        SwitchListTile(
+          title: const Text('Switch 1'),
+          value: _switchValue,
+          onChanged: (value) {
+            setState(() {
+              _switchValue = value;
+            });
+          },
+        ),
+        ExpansionTile(
+          title: const Text('Transportation'),
+          subtitle: Text('Select your ${_selectedTransportation?.name}'),
+          children: [
+            RadioListTile(
+              title: const Text('Car'),
+              value: Transportation.car,
+              groupValue: _selectedTransportation,
+              onChanged: (value) {
+                setState(() {
+                  _selectedTransportation = value;
+                });
+              },
+            ),
+
+            RadioListTile(
+              title: const Text('Bike'),
+              value: Transportation.bike,
+              groupValue: _selectedTransportation,
+              onChanged: (value) {
+                setState(() {
+                  _selectedTransportation = value;
+                });
+              },
+            ),
+            RadioListTile(
+              title: const Text('Bus'),
+              value: Transportation.bus,
+              groupValue: _selectedTransportation,
+              onChanged: (value) {
+                setState(() {
+                  _selectedTransportation = value;
+                });
+              },
+            ),
+          ],
+        ),
+       
+        ExpansionTile(
+          title: const Text('Meals'),
+          children: [
+            CheckboxListTile(
+              title: const Text('Wants Breakfast'),
+              value: _wantsBreakfast,
+              onChanged: (value) {
+                setState(() {
+                  _wantsBreakfast = value ?? false;
+                });
+              },
+            ),
+            CheckboxListTile(
+              title: const Text('Wants Lunch'),
+              value: _wantsLunch,
+              onChanged: (value) {
+                setState(() {
+                  _wantsLunch = value ?? false;
+                });
+              },
+            ),
+            CheckboxListTile(
+              title: const Text('Wants Dinner'),
+              value: _wantsDinner,
+              onChanged: (value) {
+                setState(() {
+                  _wantsDinner = value ?? false;
+                });
+              },
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
